1b43083df5dba7034e090ca8b811b0069257a028
saquino0827 <samuel.aquino@gsa.gov>
2025-08-16 18:03:20 -0500

    Finish file retrieval and framing data - next step is aggregation and saving

--- builder/main.ts
+++ builder/main.ts
@@ -2,48 +2,61 @@ import DataFrame from 'dataframe-js';
 import csv from 'csv-parser';
 import * as fs from 'fs';
 
+interface WebsiteInventory {
+    name: string;
+    file_url: string;
+}
 
-function load(inventoryPath: string, snapshotPath: string): Promise<DataFrame> {
-    const inventoryStream = fs.createReadStream(inventoryPath, { encoding: 'utf8' });
-    let currentData
-    inventoryStream.pipe(csv())
-        .on('data', async (inventory) => {
-            const agency = inventory['agency'];
-            const fileUrl = inventory['website_inventory'];
+async function downloadAndLoad(inventoryPath: string, snapshotPath: string): Promise<[DataFrame]> {
+    return new Promise((resolve, reject) => {
+        const results: WebsiteInventory[] = [];
+        const inventoryStream = fs.createReadStream(inventoryPath, {encoding: 'utf8'});
+        inventoryStream.pipe(csv())
+            .on('data', async (row: WebsiteInventory) => {
+                if (row.file_url) results.push(row);
+            })
+            .on('end', async () => {
+                try {
+                    const promises = results.map(async (inventory) => {
+                        const domain = retrieveDomainFromUrl(inventory.file_url);
+                        const savePath = snapshotPath + domain + '.csv';
 
-            currentData = await DataFrame.fromCSV(fileUrl, true);
-            currentData.toCSV(true, snapshotPath)
-        })
-        .on('end', () => {
-            console.log('Finished parsing website inventory data.');
-        })
-        .on('error', () => {
-        console.warn('There was an issue loading the CSV from ${agency}: ${error.message}');
-        })
-    return currentData    
+                        const currentData = await DataFrame.fromCSV(inventory.file_url, true);
+                        currentData.toCSV(true, savePath);
+                        return currentData
+                    });
+
+                    const websiteInventoryDataFrames = await Promise.all(promises);
+                    console.log('Finished parsing website inventory data.');
+                    return websiteInventoryDataFrames;
+                } catch (err) {
+                    reject(err);
+                }
+            })
+            .on('error', () => {
+                console.warn('There was an issue loading the CSV from ${agency}: ${error.message}. Skipping...');
+            })
+    });
+}
+
+function retrieveDomainFromUrl(url: string): string {
+    const match = url.match(/^https?:\/\/(?:www\.)?([^\/]+)/i)
+    return match ? match[1] : null;
 }
 
 async function main() {
     // load data
-    const inventoryFilePath = './website_inventories.csv'
-    const snapshotFilePath = '../snapshots/'
-    const inventoryStream = fs.createReadStream(inventoryFilePath, { encoding: 'utf8' });
-    inventoryStream.pipe(csv())
-        .on('data', (inventory) => {
-            const agency = inventory['agency'];
-            const fileUrl = inventory['website_inventory'];
+    const inventoryFilePath = './website_inventories.csv';
+    const snapshotFilePath = '../snapshots/';
 
-            let currentData = DataFrame.fromCSV(fileUrl, true);
-            currentData.toCSV(true, snapshotFilePath)
-        })
-        .on('end', () => {
-            console.log('Finished parsing website inventory data.');
-        })
-        .on('error', () => {
-           console.warn('There was an issue loading the CSV from ${agency}: ${error.message}');
-           ; 
-        })
-
+    await (async () => {
+        try {
+            const dataFramedWebsiteInventories = await downloadAndLoad(inventoryFilePath, snapshotFilePath);
+            console.log('Download and storage complete.');
+        } catch (err) {
+            console.error(err);
+        }
+    })();
 
     // sort csv
 
